08578704d98daf5622b27590208ba4645963bc4e
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2023-06-15 15:40:21 +0200

    implement improvements by Issam-b

--- android_openssl.cmake
+++ android_openssl.cmake
@@ -1,21 +1,20 @@
-function(android_add_openssl_to_targets)
+function(add_android_openssl_libraries_for_targets)
     if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-        set (SSL_ROOT_PATH ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/no-asm)
+        set (ssl_root_path ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/no-asm)
     else()
-        set (SSL_ROOT_PATH ${CMAKE_CURRENT_FUNCTION_LIST_DIR})
+        set (ssl_root_path ${CMAKE_CURRENT_FUNCTION_LIST_DIR})
     endif()
 
     if (Qt6_VERSION VERSION_GREATER_EQUAL 6.5.0)
-        list(APPEND ANDROID_EXTRA_LIBS
-            ${SSL_ROOT_PATH}/ssl_3/${CMAKE_ANDROID_ARCH_ABI}/libcrypto_3.so
-            ${SSL_ROOT_PATH}/ssl_3/${CMAKE_ANDROID_ARCH_ABI}/libssl_3.so)
+        list(APPEND android_extra_libs
+            ${ssl_root_path}/ssl_3/${CMAKE_ANDROID_ARCH_ABI}/libcrypto_3.so
+            ${ssl_root_path}/ssl_3/${CMAKE_ANDROID_ARCH_ABI}/libssl_3.so)
     else()
-        list(APPEND ANDROID_EXTRA_LIBS
-            ${SSL_ROOT_PATH}/ssl_1.1/${CMAKE_ANDROID_ARCH_ABI}/libcrypto_1_1.so
-            ${SSL_ROOT_PATH}/ssl_1.1/${CMAKE_ANDROID_ARCH_ABI}/libssl_1_1.so)
+        list(APPEND android_extra_libs
+            ${ssl_root_path}/ssl_1.1/${CMAKE_ANDROID_ARCH_ABI}/libcrypto_1_1.so
+            ${ssl_root_path}/ssl_1.1/${CMAKE_ANDROID_ARCH_ABI}/libssl_1_1.so)
     endif()
 
-    #message(WARNING "set_target_properties(${ARGV} PROPERTIES QT_ANDROID_EXTRA_LIBS \"${ANDROID_EXTRA_LIBS}\")")
-    set_target_properties(${ARGV} PROPERTIES
-        QT_ANDROID_EXTRA_LIBS "${ANDROID_EXTRA_LIBS}")
+    set_target_properties(${ARGN} PROPERTIES
+        QT_ANDROID_EXTRA_LIBS "${android_extra_libs}")
 endfunction()
